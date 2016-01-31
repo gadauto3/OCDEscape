47683400b8da7adef2e47fa0796902852360c865
Michael Jones <mike@tacticalhaptics.com>
2016-01-31 02:07:03 -0800

    Made scribblers work nice

---
+++ Assets/OCDRoomEscape/Scripts/InteractionProxy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionProxy : InteractableObject
+{
+    public InteractableObject interactable;
+
+    public override void OnHighlight(GazePointer pointer)
+    {
+        interactable.OnHighlight(pointer);
+    }
+
+    public override void OffHighlight(GazePointer pointer)
+    {
+        interactable.OffHighlight(pointer);
+    }
+
+    public override bool OnInteract(GazePointer pointer)
+    {
+        return interactable.OnInteract(pointer);
+    }
+
+    public override bool OnGrabbedHighlight(Transform highlightedObject)
+    {
+        return interactable.OnGrabbedHighlight(highlightedObject);
+    }
+
+    public override bool OffInteract(GazePointer pointer, Transform objectToInteractWith)
+    {
+        return interactable.OffInteract(pointer, objectToInteractWith);
+    }
+
+    public override void LogicUpdate()
+    {
+        interactable.LogicUpdate();
+    }
+
+    // Fixed Update, called by the GazePointer
+    public override void PhysicsUpdate()
+    {
+        interactable.PhysicsUpdate();
+    }
+}
