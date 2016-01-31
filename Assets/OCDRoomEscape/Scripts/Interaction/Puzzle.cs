bf8d386450408c1d6144e4fdf97aa283e74da801
Gabriel Adauto <gabriel@motionmathgames.com>
2016-01-31 02:13:39 -0800

    Set up initial PuzzleMaster code

---
+++ Assets/OCDRoomEscape/Scripts/Interaction/Puzzle.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puzzle : MonoBehaviour {
+	
+	float waitBeforeTryingToReset = 8f;
+	float waitBetweenResetChecks = 2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public virtual bool IsPuzzleComplete() {
+		return false;
+	}
+	
+	public virtual bool IsResetable() {
+		return false;
+	}
+
+	public virtual void MarkForReset() {
+		StartCoroutine(WaitForPlayerToLookAwayToReset());
+	}
+	
+	public virtual void ResetPuzzle() {
+		
+	}
+
+	private IEnumerator WaitForPlayerToLookAwayToReset()
+	{
+		yield return new WaitForSeconds(waitBeforeTryingToReset);
+
+		while (IsPuzzleInCameraView()) {
+			yield return new WaitForSeconds(waitBetweenResetChecks);
+		}
+
+		ResetPuzzle();
+	}
+
+	private bool IsPuzzleInCameraView()
+	{
+		// TODO: Define
+	}
+}
---
+++ Assets/OCDRoomEscape/Scripts/Interaction/PuzzleMaster.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PuzzleMaster : MonoBehaviour 
+{
+	public List<Puzzle> puzzles;
+	float increment;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		puzzles = new List<Puzzle>();
+	}
+
+	void Start()
+	{
+		// This is how much the room should grow by on each puzzle completion
+		increment = 1f / puzzles.Count;
+	}
+	
+	public void PuzzleCompleted(Puzzle puzzle)
+	{
+		// Notify room of growth increment
+
+		// Remove the puzzle, but also allow it to remain in the list
+		puzzles.RemoveAt(puzzles.IndexOf(puzzle));
+
+		if (puzzle.IsResetable && puzzles.Contains(puzzle)) {
+			puzzle.MarkForReset();
+		}
+	}
+}
