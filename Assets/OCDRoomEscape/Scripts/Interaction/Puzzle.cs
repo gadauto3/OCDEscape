87bbf3de6acda4ebf42487003307adbf795f6052
Gabriel Adauto <gabriel@motionmathgames.com>
2016-01-31 14:06:54 -0800

    updated puzzle

--- Assets/OCDRoomEscape/Scripts/Interaction/Puzzle.cs
+++ Assets/OCDRoomEscape/Scripts/Interaction/Puzzle.cs
@@ -6,7 +6,7 @@ public class Puzzle : MonoBehaviour {
 	public PuzzleMaster master;
 	public int puzzleWeight = 1; // If the puzzle has more weight, it has a greater affect on room growth
 
-	float waitBeforeTryingToReset = 8f;
+	float waitBeforeTryingToReset = 1f;
 	float waitBetweenResetChecks = 2f;
 
 	// Use this for initialization
